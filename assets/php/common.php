edfddb597e88d779dffc48d9b4b9b804c5898d95
Nepaltechguy2 <nepalsamyok@gmail.com>
2017-10-26 22:10:05 -0500

    init

---
+++ assets/php/common.php
@@ -0,0 +1,34 @@
+<?php
+
+$head = '
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <style>
+body, html {
+	margin:0;
+}
+#backgrounddiv {
+	background: #000000 url("http://samyok.us/public/assets/blackboard.png") repeat-y center;
+	    background-size: 100%;
+	opacity: 0.2;
+    filter: alpha(opacity=50); /* For IE8 and earlier */
+	color: white;
+	z-index: -1;
+	position: fixed;
+	top:0;
+	left:0;
+	height: 100%; width: 100%;
+}
+</style>
+';
+
+
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,32 @@
+include 'assets/php/common.php'
+<!DOCTYPE html>
+<html>
+<head>
+<?php
+session_start();
+echo $head;
+?>
+	<title>Halloween AMC8</title>
+</head>
+<body>
+
+<div class="w3-card-4">
+
+<div class="w3-container w3-dark-red">
+  <h2>Submit Answers to Halloween Mock AMC 8!</h2>
+</div>
+
+<form class="w3-container">
+
+<label>First Name</label>
+<input class="w3-input" type="text">
+
+<label>Last Name</label>
+<input class="w3-input" type="text">
+
+</form>
+
+</div>
+
+</body>
+</html>
