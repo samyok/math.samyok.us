bc8abe44ef60fdef2492b077a360c79d018f1492
Nepaltechguy2 <nepalsamyok@gmail.com>
2017-10-27 18:19:57 -0500

    switch arg order for bug fix

--- login.php
+++ login.php
@@ -24,9 +24,12 @@ if(isset($_POST['name'])){
 						echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
 					}
 				} else {
-			if (!in_array($_POST['name'], $_SESSION['names'])) {
-				if(isset($_SESSION['names'])){$_SESSION['names'] = array();}
+			if (isset($_SESSION['names'])) {
+				if(!in_array($_POST['name'], $_SESSION['names'])){
 				$_SESSION['names']=array_push( $_SESSION['names'], $name);
+				}
+			} else {
+				$_SESSION['names'] = array();
 			}
 		}
 }
