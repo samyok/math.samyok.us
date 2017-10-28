ea2f9ea103a01f89443f95812f94de7cb4434cac
Nepaltechguy2 <nepalsamyok@gmail.com>
2017-10-28 18:06:12 -0500

    fix time limit

--- login.php
+++ login.php
@@ -30,7 +30,7 @@ if($name === NULL){
 	echo "E_NO_NAME_ENTERED";
 	exit();
 }
-echo "<script>$('html, body').animate({ scrollTop: 0 }, 1000);$('.hero-image').hide('slow');</script><h1>$name, you have 45 minutes. Begin.</h1>" ;
+echo "<script>$('html, body').animate({ scrollTop: 0 }, 1000);$('.hero-image').hide('slow');</script><h1>$name, you have 40 minutes. Begin.</h1>" ;
 echo "<p>Click your answer. Correct answers are worth 1, incorrect ones are worth 0. Click <a href='http://aops.com'>here</a> if you do not have a copy of the test. </p>";
 echo "<ol>";
 $i = 1;
