496882971dbe134bc22d6cf5fce6c27790e1f60b
Nepaltechguy2 <nepalsamyok@gmail.com>
2017-10-29 18:04:27 -0500

    now we replace forward slashes

--- feedback.php
+++ feedback.php
@@ -16,7 +16,7 @@ if(!isset($_POST['favprob']) && !isset($_POST['rating']) && !isset($_POST['impre
 	header("Location: index.php");
 	exit();
 }
-$name= stripslashes(htmlspecialchars($_SESSION['names']));
+$name= str_replace('/','',stripslashes(htmlspecialchars($_SESSION['names'])));
 $fback = $name." said: \n\nMy favorite problem was ".htmlspecialchars($_POST['favprob']).". \n \nI rated the difficulty as ".htmlspecialchars($_POST['rating'])." out of 5. \n \nHow we can improve: ".htmlspecialchars($_POST['impre'])."Creativity: ".htmlspecialchars($_POST['creat']);
 $newfilename = time()."-".$name.".txt";
 $newFile = fopen($newfilename, 'w') or die('Sorry! Your feedback was not saved. If there is something you need to tell us, email me at <a href="mailto:samyok@samyok.us">samyok@samyok.us</a>. Click <a href="index.php">here</a> to go back.
