8e1819fc8401dbbed5fbbef7008bb238069221dd
Nepaltechguy2 <nepalsamyok@gmail.com>
2017-10-29 18:21:09 -0500

    mere typos

--- feedback.php
+++ feedback.php
@@ -17,11 +17,12 @@ if(!isset($_POST['favprob']) && !isset($_POST['rating']) && !isset($_POST['impre
 	exit();
 }
 $name= str_replace('/','',stripslashes(htmlspecialchars($_SESSION['names'])));
-$fback = $name." said: \n\nMy favorite problem was ".htmlspecialchars($_POST['favprob']).". \n \nI rated the difficulty as ".htmlspecialchars($_POST['rating'])." out of 5. \n \nHow we can improve: ".htmlspecialchars($_POST['impre'])."Creativity: ".htmlspecialchars($_POST['creat']);
-$newfilename = time()."-".$name.".txt";
+$fback = $name." said: \n\nMy favorite problem was ".htmlspecialchars($_POST['favprob']).". \n \nI rated the difficulty as ".htmlspecialchars($_POST['rating'])." out of 5. \n \nHow we can improve: ".htmlspecialchars($_POST['impre'])."\n\nCreativity: ".htmlspecialchars($_POST['creat']);
+$newfilename = "reviews/".time()."-".$name.".txt";
 $newFile = fopen($newfilename, 'w') or die('Sorry! Your feedback was not saved. If there is something you need to tell us, email me at <a href="mailto:samyok@samyok.us">samyok@samyok.us</a>. Click <a href="index.php">here</a> to go back.
 <br> <br> Error code: '.$newfilename);
 
-fwrite($newfile, $fback);
+$saaa = fwrite($newFile, $fback);
+echo $saaa;
 header("Location: index.php");
 ?>
