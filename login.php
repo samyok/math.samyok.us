789daae2b1630699e1b53d15e94fb22e41671068
Nepaltechguy2 <nepalsamyok@gmail.com>
2017-10-29 11:33:47 -0500

    again less a go

--- login.php
+++ login.php
@@ -30,8 +30,8 @@ if($name === NULL){
 	echo "E_NO_NAME_ENTERED";
 	exit();
 }
-echo "<div id='loadanswer' class=\"load\" style=\"display:none;\"></div>
-</div><div id='answerSheethide'><script>$('html, body').animate({ scrollTop: 0 }, 1000);$('.hero-image').hide('slow');</script><h1>$name, you have 40 minutes. Begin.</h1>" ;
+echo "<script>$('html, body').animate({ scrollTop: 0 }, 1000);$('.hero-image').hide('slow');</script><div id='loadanswer' class=\"load\" style=\"display:none;\"></div>
+</div><div id='answerSheethide'><h1>$name, you have 40 minutes. Begin.</h1>" ;
 echo "<p>Click your answer. Correct answers are worth 1, incorrect ones are worth 0. Click <a href='http://aops.com'>here</a> if you do not have a copy of the test. </p>";
 echo "<ol>";
 $i = 1;
