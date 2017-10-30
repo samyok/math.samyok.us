d7be476b9265ff79e7e163c0c61dbd87b500a01c
Nepaltechguy2 <nepalsamyok@gmail.com>
2017-10-29 20:55:44 -0500

    info text

--- viewScores.php
+++ viewScores.php
@@ -25,9 +25,9 @@ if ($result->num_rows > 0) {
 	echo "<script>$('html, body').animate({ scrollTop: 0 }, 1000);$('.hero-image').hide('slow');</script><style>body{overflow: auto}</style>";
     while($row = $result->fetch_assoc()) {
         echo "
-			<h1>Hello again, ".$row["name"].".</h1>";
+			<h1>Hello again, ".$row["name"].".</h1><p>Green and black highlights what problems you got wrong (black is what you answered, green is the correct answer), yellow highlights unanswered questions, and black only highlights are correct. Click <a href='printout.pdf'>here</a> to see the problems.";
 echo "<button style='width: 50%;' onclick='window.location.reload()' class='black-hover'>Home</button><button style='width: 50%;' onclick='window.location.href=".'"leaderboard.php"'." class='lack-hover'>Leaderboard</button>";
-		if($row['showLeaderboard']===0){echo "
+		if($row['showLeaderboard']==0){echo "
 			<p>You have chosen <b>NOT</b> to show your name on the leaderboard. To change this, you will need to either <a href='samyok@samyok.us'>email me</a> or PM me (techguy2). </p>";
 		}
 		$correctAnswers = "B, B, D, C, D, C, D, D, A, B, E, C, E, A, D, D, E, D, B, D, E, A, D, C, E";
