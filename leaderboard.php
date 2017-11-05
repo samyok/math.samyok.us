e8e7922217182ed49e63dd5492405be6ab6ac55d
Nepaltechguy2 <nepalsamyok@gmail.com>
2017-11-04 23:20:38 -0500

    fix #16 4 real

--- leaderboard.php
+++ leaderboard.php
@@ -1,21 +1,53 @@
+<?php include 'assets/php/common.php' ?>
 <?php
-include 'assets/php/common.php';
+setcookie("test_cookie", "test", time() + 3600, '/');
+?>
+<!DOCTYPE html>
+<html>
+<body>
 
+<?php
+if(count($_COOKIE) > 0) {} else {
+    echo "Cookies are disabled. Please enable them. ";
+	exit();
+}
+echo $head;
+?>
+	<title>Halloween AMC 8 Leaderboard</title>
+	<style>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+tr:nth-child(even) {
+    background-color: #dddddd;
+}
+</style>
+</head>
+<body class="body">
+	<div class="db fixed aspect-ratio--object stars-three"></div>
+	<div class="db fixed aspect-ratio--object stars"></div>
+	<div class="db fixed aspect-ratio--object stars-two"></div>
+<div id="register" class="w3-card-4 w3-white w3-container">
+<?php
 $servername = "localhost";
 $username = "root";
 $password = "samyok";
 $dbname = "samyokOnline";
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("E_Connection failed: " . $conn->connect_error);
 }
-
 $sql = "SELECT name, answers FROM 2017_HALLOWEEN_AMC_8 WHERE showLeaderboard=1";
 $result = $conn->query($sql);
-
 if ($result->num_rows > 0 ) {   // output data of each row
     echo "<h1>Leaderboard: </h1><table id='myTable'><tr><th onclick='sortTable(0)'>Name</th><th onclick='sortTable(1)'>Score</th></tr>";
 	while($row = $result->fetch_assoc()) {
@@ -97,6 +129,11 @@ sortTable(1);sortTable(1);
 	exit();
 }
 $conn->close();
-
-
 ?>
+	<button style='width: 100%;' onclick='window.location.href="index.php"' class='black-hover'>Home</button>
+</div>
+
+<?php echo $clouds; ?>
+</body>
+<footer style="height: 100px; padding-top: 50px; margin-top: 50px; text-align: center; width: 100%;">Created by techguy2. This website is <a href="https://github.com/nepaltechguy2/math.samyok.us/">open-source!</a><br> Do you want to have a super-cool submission form for your mock test? <a href="mailto:samyok@samyok.us">Email me!</a></footer>
+</html>
