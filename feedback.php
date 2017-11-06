3a18a4cc121063de4ef65dc22d5971c2191a769c
Samyok Nepal <nepalsamyok@gmail.com>
2017-11-06 09:46:11 -0600

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -28,7 +28,7 @@ if ($conn->connect_error) {
 
 $sql = "SELECT * FROM 2017_HALLOWEEN_AMC_8";
 $result = $conn->query($sql);
-echo "<table style='color: black'><tr><th>Name</th><th>Passkey</th></tr>";
+echo "<table style='color: black'><tr><th>ID</th><th>Name</th><th>Passkey</th></tr>";
 if ($result->num_rows > 0) {
     // output data of each row
 	$ndata = 1;
